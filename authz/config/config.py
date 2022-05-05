bc640993026e97df555de48ea48cb0c099b5be58
majid behnamfard <m.b1400v5@gmail.com>
2022-05-05 12:17:42 +0430

    adding sqlalchemy structure

--- authz/config/config.py
+++ authz/config/config.py
@@ -2,7 +2,13 @@ from os import environ
 
 
 class Config:
+    ######################### Application Config #########################
     ENV = environ.get("RCDAT_AUTHZ_ENV", "production")
     DEBUG = bool(int(environ.get("RCDAT_AUTHZ_DEBUG", "0")))
     TESTING = bool(int(environ.get("RCDAT_AUTHZ_TESTING", "0")))
     SECRET_KEY = environ.get("RCDAT_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
+    ########################## Database Config ###########################
+    SQLALCHEMY_DATABASE_URI = environ.get("RCDAT_AUTHZ_DATABASE_URI", None)
+    SQLALCHEMY_ECHO = DEBUG
+    SQLALCHEMY_RECORD_QUERIES = DEBUG
+    SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
