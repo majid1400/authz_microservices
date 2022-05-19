7d33800f97d970fc88a6e8ce154f16c2eec7aa76
majid behnamfard <m.b1400v5@gmail.com>
2022-05-19 22:41:55 +0430

    update user controller

--- authz/controller/apiv1/user.py
+++ authz/controller/apiv1/user.py
@@ -1,24 +1,125 @@
+from flask import request
+from authz.authz import db
 from authz.model import User
 from authz.schema.apiv1 import UserSchema
-from authz.util.jsonify import jsonify
+from authz.util import jsonify, user_expires_at, now
 
 
 class UserController:
     def get_users():
-        users = User.query.all()
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)
+        try:
+            users = User.query.all()
+        except Exception as e:
+            return jsonify(status=500, code=102)
         users_schema = UserSchema(many=True)
-        return {
+        return jsonify({
             "users": users_schema.dump(users)
-        }
+        })
 
     def get_user(user_id):
-        return jsonify(status=501, code=107)
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            return jsonify(status=500, code=102)
+        if user is None:
+            return jsonify(status=404, code=103)
+        users_schema = UserSchema()
+        return jsonify({
+            "users": users_schema.dump(user)
+        })
 
     def create_user():
-        return jsonify(status=501, code=107)
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)
+        users_schema = UserSchema(only=["username", "password"])
+        try:
+            user_data = users_schema.load(request.get_json())
+        except:
+            return jsonify(status=400, code=104)
+        if not user_data.get("username") or not user_data.get("password"):
+            return jsonify(status=400, code=105)
+        try:
+            user = User.query.filter_by(username=user_data.get("username")).first()
+        except Exception as e:
+            return jsonify(status=500, code=102)
+        if user is not None:
+            return jsonify(status=409, code=106)
+        user = User(
+            username=user_data.get("username"),
+            password=user_data.get("password"),
+        )
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify(status=500, code=102)
+        user_schema = UserSchema()
+        return jsonify(
+            {"user": user_schema.dump(user)}, status=201
+        )
 
     def update_user(user_id):
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)
+        users_schema = UserSchema(only=["password"], unknown="include")
+        try:
+            user_data = users_schema.load(request.get_json())
+        except Exception as e:
+            return jsonify(status=400, code=104)
+        if len(user_data) != 2:
+            return jsonify(status=400, code=104)
+        if "password" not in user_data or "oldpassword" not in user_data:
+            return jsonify(status=400, code=104)
+        if not user_data.get("password") or not user_data.get("oldpassword"):
+            return jsonify(status=400, code=105)
+
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            return jsonify(status=500, code=102)
+
+        if user is None:
+            return jsonify(status=404, code=103)
+        if user.password != user_data.get("oldpassword"):
+            return jsonify(status=403, code=111)
+
+        user.password = user_data.get("password")
+        user.expires_at = user_expires_at()
+        user.last_change_at = now()
+        user.failed_auth_count = 0
+        user.failed_auth_at = None
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify(status=500, code=102)
+        user_schema = UserSchema()
+        return jsonify(
+            {"user": user_schema.dump(user)}
+        )
+
         return jsonify(status=501, code=107)
 
     def delete_user(user_id):
-        return jsonify(status=501, code=107)
+        if request.content_type != "application/json":
+            return jsonify(status=415, code=101)
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            return jsonify(status=500, code=102)
+        if user is None:
+            return jsonify(status=404, code=103)
+
+        db.session.delete(user)
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify(status=500, code=102)
+        return jsonify()
+
