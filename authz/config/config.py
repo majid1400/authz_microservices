6df723a5f248196f17f92500c2887a627da09cde
majid behnamfard <m.b1400v5@gmail.com>
2022-05-26 19:52:24 +0430

    update authentication endpoint

--- authz/config/config.py
+++ authz/config/config.py
@@ -8,12 +8,17 @@ class Config:
     TESTING = bool(int(environ.get("RCDAT_AUTHZ_TESTING", "0")))
     SECRET_KEY = environ.get("RCDAT_AUTHZ_SECRET_KEY", "HARD-HARD-HARD-SECRET-KEY")
     TIMEZONE = environ.get("RCDAT_AUTHZ_TIMEZONE", "Asia/Tehran")
+    JWT_ALGO = environ.get("RCDAT_AUTHZ_JWT_ALGO", "HS512")
     ########################## Database Config ###########################
     SQLALCHEMY_DATABASE_URI = environ.get("RCDAT_AUTHZ_DATABASE_URI", None)
     SQLALCHEMY_ECHO = DEBUG
     SQLALCHEMY_RECORD_QUERIES = DEBUG
     SQLALCHEMY_TRACK_MODIFICATIONS = DEBUG
     ############################# User Config #############################
+    USER_ENABLED_STATUS = 1
+    USER_ACTIVATED_STATUS = 2
+    USER_ALL_STATUS = 3
+    USER_DEFAULT_TOKEN_EXPIRY_TIME = int(environ.get("RCDAT_AUTHZ_USER_DEFAULT_TOKEN_EXPIRY_TIME", "86400"))
     USER_DEFAULT_ROLE = environ.get("RCDAT_AUTHZ_USER_DEFAULT_ROLE", "member")
     USER_DEFAULT_EXPIRY_TIME = int(environ.get("RCDAT_AUTHZ_USER_DEFAULT_EXPIRY_TIME", "365"))
     USER_DEFAULT_STATUS = int(environ.get("RCDAT_AUTHZ_USER_DEFAULT_STATUS", "3"))
